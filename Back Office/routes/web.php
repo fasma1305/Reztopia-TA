eb4ff14ad5bf787fab39c86c0a0d63c3fde5b0bb
fasma1305 <fasma.rega@gmail.com>
2023-06-05 09:41:59 +0700

    sprint1-5

--- Back Office/routes/web.php
+++ Back Office/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(
+    [
+        'prefix' => 'auth'
+        // 'middleware' => 'auth', 'role:admin'
+    ],
+    function () {
+        Route::get('login', [AuthController::class, 'login'])->name('login');
+        Route::post('post_login', [AuthController::class, 'post_login'])->name('post_login');
+        Route::get('forgot-password', [AuthController::class, 'forgot'])->name('forgot-password');
+        // Route::get('register', [AuthController::class, 'register'])->name('register');
+        // Route::post('register', [AuthController::class, 'post_register'])->name('post_register');
+    }
+);
+
